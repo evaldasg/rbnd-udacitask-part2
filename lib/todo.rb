6a656b6c5f0e9fc035a419e661a86af4098249df
Evaldas Grudzinskas <prohdo@icloud.com>
2016-07-08 14:40:12 +0300

    Add InvalidPriorityValue error and validate_and_assign_priority method for todolist

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,17 +1,27 @@
 class TodoItem
   include Listable
 
+  ALLOWED_PRIORITIES = %w(low medium high).freeze
+
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
     @description = description
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
-    @priority = options[:priority]
+    validate_and_assign_priority(options[:priority]) if options[:priority]
   end
 
   def details
     format_description(@description) + 'due: ' +
       (@due ? format_date(@due) : 'No due date') +
       format_priority(@priority)
+  end
+
+  private
+
+  def validate_and_assign_priority(priority)
+    message = "'#{priority}' is not allowed."
+    raise UdaciListErrors::InvalidPriorityValue, message unless ALLOWED_PRIORITIES.include?(priority)
+    @priority = priority
   end
 end
